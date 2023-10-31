3314150ae72c7c0627dc400b739f60c1a9c85bee
spokiesparsh <jainsparsh5@gmail.com>
2023-10-31 20:54:30 +0000

    new commit for data 02_dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345
+let accountEmail = "sparsh@gmail.com"
+var accountPass = "Sparshjain.123"
+accountCity = "Jaipur"
+// accountId = 2
+
+accountEmail = "assad@gmail.com"
+
+
+console.log(accountId)
+
+console.table([accountEmail,accountId,accountPass,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// data types
+
+let name = "Sparsh" 
+// string = ""
+
+
+//number // if number too big we may also use bigint
+
+//boolean => true/false
+
+
+// null => its a standalone value
+
+//undefines => when value is not assigned for now
+
+//symbol => to find uniqueness 
+
+
+//objects 
+
+// will give object
+console.log(typeof null)
+
+//will give undefined only 
+console.log(typeof undefined)
+
+
+
+
+
