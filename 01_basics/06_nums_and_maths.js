b5626b1d1710603e983f73ec18f18bc87a032374
spokiesparsh <jainsparsh5@gmail.com>
2023-11-07 19:25:42 +0000

    primitive data types last chapter

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+const score = 400
+const balance = new Number(100)
+
+// console.log(balance)
+// console.log(score)
+
+// console.log(balance.toFixed(2))
+
+const otherNum = 23.7866
+
+// console.log(typeof(otherNum.toPrecision(4)))
+
+const hundred = 1000000
+
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+// --------------------- Maths ----------------------- 
+
+// console.log(Math.abs(-1))
+
+// console.log(Math.round(4.2))
+
+// console.log(Math.ceil(4.3))
+
+// console.log(Math.floor(4.2))
+
+
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,23 @@
+let myDate = new Date()
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+
+
+let myCreatedDate = new Date(2023,10,8)
+console.log(myCreatedDate)
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+
+console.log(Math.floor(Date.now()/1000))
+
+
+let newDate = new Date()
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
