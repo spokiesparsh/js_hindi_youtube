0a25c9ca09bcfdbb3f4c45e64e74799ffe304e0c
SparshJain <jainsparsh17@gmail.com>
2023-11-16 14:37:16 +0530

    nothing just trying my local

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
